c282ef69ec73623d0d7d96b3c267a7853b3c50e5
windowcow <65646883+windowcow@users.noreply.github.com>
2022-09-01 16:48:53 +0900

    first commit

---
+++ quiz1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int *pi, *pj;
+    
+    int list[10][10];
+    list[0] = 1;
+    printf("%d", list[0]);
+    printf("%d", list);
+    // printf(list);
+    return 0;
+} 
