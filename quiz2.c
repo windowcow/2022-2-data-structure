9f6e936d424ee709e9de3788519c0c83b94b5dc4
windowcow <65646883+windowcow@users.noreply.github.com>
2022-09-01 18:37:12 +0900

    안됨

---
+++ quiz2.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int list[3]; // 어차피 3개인거 알기 때문에 이렇게 함
+    int x, y, z;
+
+    x = rand() % 123;
+    y = rand() % 123;
+    z = rand() % 123;
+
+    list[0] = x;
+    list[1] = y;
+    list[2] = z;
+
+    printf("<정렬 전> : %d, %d, %d\n", list[0], list[1], list[2]);
+
+    // 무작위 상태
+
+    if (x >= y)
+    {
+        // [y, x, _] 의 상태가 되도록
+        list[0] = y;
+        list[1] = x;
+    }
+    else
+    {
+        list[0] = x;
+        list[1] = y;
+    }
+
+    // list[0], list[1]의 대소는 가려진 상태
+
+    if (list[2] >= list[1])
+    {
+        printf("<정렬 후> : %d, %d, %d", list[0], list[1], list[2]);
+        return 0;
+    }
+
+    if (list[1] > list[2] && list[2] >= list[0])
+    {
+        int tmp = list[2];
+        list[2] = list[1];
+        list[1] = tmp;
+        printf("<정렬 후> : %d, %d, %d", list[0], list[1], list[2]);
+        return 0;
+    }
+    if (list[2] < list[0])
+    {
+        int tmp1 = list[0];
+        int tmp2 = list[1];
+        list[0] = list[2];
+        list[1] = list[tmp1];
+        list[2] = list[tmp2];
+        printf("<정렬 후> : %d, %d, %d", list[0], list[1], list[2]);
+        return 0;
+    }
+
+    if (z >= x)
+    {
+        // [y, x, z]
+        list[2] = z;
+        printf("<정렬 후> : %d, %d, %d", list[0], list[1], list[2]);
+        return 0;
+    }
+
+    if (z >= y)
+    {
+        list[2] = x;
+        list[1] = z;
+        list[0] = y;
+
+        printf("<정렬 후> : %d, %d, %d", list[0], list[1], list[2]);
+        return 0;
+    }
+
+    if (y >= z)
+    {
+        list[2] = x;
+        list[1] = y;
+        list[0] = z;
+
+        printf("<정렬 후> : %d, %d, %d", list[0], list[1], list[2]);
+        return 0;
+    }
+
+    return 0;
+}
